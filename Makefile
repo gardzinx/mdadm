3a7a3a26ab193c709c7fc16d7468637161c7e28e
Gardzinski, RomanX <romanx.gardzinski@intel.com>
2026-09-10 13:41:52 +0200

    Add list implementation to project

--- Makefile
+++ Makefile
@@ -185,7 +185,7 @@ OBJS = mdadm.o config.o policy.o mdstat.o  ReadMe.o uuid.o util.o maps.o lib.o u
        mdopen.o super0.o super1.o super-ddf.o super-intel.o bitmap.o \
        super-mbr.o super-gpt.o \
        restripe.o sysfs.o sha1.o mapfile.o crc32.o msg.o xmalloc.o \
-       platform-intel.o probe_roms.o crc32c.o drive_encryption.o
+       platform-intel.o probe_roms.o crc32c.o drive_encryption.o list.o
 
 CHECK_OBJS = restripe.o uuid.o sysfs.o maps.o lib.o xmalloc.o dlink.o
 
@@ -198,7 +198,7 @@ MON_OBJS = mdmon.o monitor.o managemon.o uuid.o util.o maps.o mdstat.o sysfs.o c
 	Kill.o dlink.o ReadMe.o super-intel.o \
 	super-mbr.o super-gpt.o \
 	super-ddf.o sha1.o crc32.o msg.o bitmap.o xmalloc.o \
-	platform-intel.o probe_roms.o crc32c.o drive_encryption.o
+	platform-intel.o probe_roms.o crc32c.o drive_encryption.o list.o
 
 MON_SRCS = $(patsubst %.o,%.c,$(MON_OBJS))
 
---
+++ list.c
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2022 Intel Corporation.
+
+/* System headers */
+#include <assert.h>
+#include <stdbool.h>
+
+/* Local headers */
+#include "list.h"
+
+void __list_erase(struct list *list, item_free_t free_fn)
+{
+	struct node *node;
+
+	list_for_each_node(list, node) {
+		if (free_fn)
+			free_fn(node->item);
+		free(node);
+	}
+	list->head = list->tail = NULL;
+}
+
+void __list_remove(struct node *node, item_free_t free_fn)
+{
+	struct list *list = node->list;
+
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+		list->head = node->next;
+	if (node->next)
+		node->next->prev = node->prev;
+	else
+		list->tail = node->prev;
+	node->list = NULL;
+	node->next = NULL;
+	node->prev = NULL;
+
+	if (free_fn)
+		free_fn(node->item);
+}
+
+bool list_insert(struct list *list, void *item, struct node *after)
+{
+	struct node *new;
+	struct node **x;
+
+	assert(item != NULL);
+	new = malloc(sizeof(struct node));
+	if (!new)
+		return false;
+
+	new->list = list;
+	new->item = item;
+
+	if (after) {
+		assert(list == after->list);
+		x = &after->next;
+	} else {
+		x = &list->head;
+	}
+
+	if (*x == NULL)
+		list->tail = new;
+	else
+		(*x)->prev = new;
+	new->next = *x;
+	*x = new;
+	new->prev = after;
+	return true;
+}
+
+bool list_insert_compar(struct list *list, void *item, bool compar_fn(void *item1, void *item2))
+{
+	struct node *tmp = NULL;
+
+	list_for_each_node(list, tmp) {
+		if (compar_fn(item, tmp->item) == true)
+			return list_insert(list, item, list_prev(tmp));
+	}
+	return list_append(list, item);
+}
